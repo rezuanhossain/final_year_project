dae7774a21efaab4b63e4dc3d3ed44d0aa5728a8
rezuanhossain <rezuanhossain956@gmail.com>
2021-08-05 19:36:51 +0600

    text

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Str;
 |
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
+| model instances for testing / seeding your application's database..
 |
 */
 
