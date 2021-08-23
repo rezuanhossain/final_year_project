af22c529b7fcd05ee68381f228896d5b09a51a56
rezuanhossain <rezuanhossain956@gmail.com>
2021-08-24 00:23:38 +0600

    working on students interest module

---
+++ app/Http/Controllers/InterestSurveyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InterestSurvey;
+use Illuminate\Http\Request;
+
+class InterestSurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InterestSurvey  $interestSurvey
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InterestSurvey $interestSurvey)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InterestSurvey  $interestSurvey
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InterestSurvey $interestSurvey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InterestSurvey  $interestSurvey
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InterestSurvey $interestSurvey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InterestSurvey  $interestSurvey
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InterestSurvey $interestSurvey)
+    {
+        //
+    }
+}
---
+++ app/InterestSurvey.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InterestSurvey extends Model
+{
+    protected $fillable = [
+        'categories', 'sub_categories','student_id'
+    ];
+
+    protected $casts = [
+        'categories' => 'array',
+        'sub_categories' => 'array'
+    ];
+}
